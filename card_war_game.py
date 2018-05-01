899a773a9de3b43be6f8da297b4e50e768d08e5f
harshgangwar <harshgangwar.rc@gmail.com>
2018-05-02 02:47:52 +0530

    Added a new file

---
+++ card_war_game.py
@@ -0,0 +1,28 @@
+
+
+from random import shuffle
+
+suite = 'H D S C'.split()
+ranks = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+
+class Deck:
+    """
+    It will be used to make a deck from suite and rank.
+    Later this deck will be used to divide the equal no. of cards among the players.
+    And then it will shuffle the deck as well
+    """
+
+class Hand:
+    """
+    This is for the players. Each player has a Hand and can add or remove cards from that hand.
+    So we will perform the add and remove operation here.
+    """
+
+
+class Player:
+    """
+    This class will has the name of the player.
+    It will also has the instance of Hand class object.
+    """
+
+print "Let's start the game"
