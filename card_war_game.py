5f7b5c6409bb30165acc2f9d32049264515f06fc
harshgangwar <harshgangwar.rc@gmail.com>
2018-05-03 01:07:57 +0530

    Added game logic

--- card_war_game.py
+++ card_war_game.py
@@ -12,11 +12,33 @@ class Deck:
     And then it will shuffle the deck as well
     """
 
+    def __init__(self):
+        print("Creating New Ordered Deck")
+        self.allcards = [(s,r) for s in suite for r in ranks ]
+
+    def shuffle(self):
+        print("Shuffling Deck")
+        shuffle(self.allcards)
+
+    def split_in_half(self):
+        return (self.allcards[:26],self.allcards[26:])
+
 class Hand:
     """
     This is for the players. Each player has a Hand and can add or remove cards from that hand.
     So we will perform the add and remove operation here.
     """
+    def __init__(self,cards):
+        self.cards = cards
+
+    def __str__(self):
+        return "Contains {} cards".format(len(self.cards))
+
+    def add(self,added_cards):
+        self.cards.extend(added_cards)
+
+    def remove_card(self):
+        return self.cards.pop()
 
 
 class Player:
@@ -25,4 +47,101 @@ class Player:
     It will also has the instance of Hand class object.
     """
 
+    def __init__(self,name,hand):
+        self.name = name
+        self.hand = hand
+
+    def play_card(self):
+        drawn_card = self.hand.remove_card()
+        print("{} has placed: {}".format(self.name,drawn_card))
+        print('\n')
+        return drawn_card
+
+    def remove_war_cards(self):
+        war_cards = []
+        if len(self.hand.cards) < 3:
+            return war_cards
+        else:
+            for x in range(3):
+                war_cards.append(self.hand.cards.pop())
+            return war_cards
+
+    def still_has_cards(self):
+        """
+        Returns True if player still has cards
+        """
+        return len(self.hand.cards) != 0
+
 print "Let's start the game"
+
+
+# Create New Deck and split in half
+d = Deck()
+d.shuffle()
+half1,half2 = d.split_in_half()
+
+# Create Both Players
+comp = Player("computer",Hand(half1))
+name = input("What is your name player? ")
+user = Player(name,Hand(half2))
+
+# Set Round Count
+total_rounds = 0
+war_count = 0
+# Let's play
+while user.still_has_cards() and comp.still_has_cards():
+    total_rounds += 1
+    print("It is time for a new round!")
+    print("Here are the current standings: ")
+    print(user.name+" count: "+str(len(user.hand.cards)))
+    print(comp.name+" count: "+str(len(comp.hand.cards)))
+    print("Both players play a card!")
+    print('\n')
+
+    # Cards on Table represented by list
+    table_cards = []
+
+    # Play cards
+    c_card = comp.play_card()
+    p_card = user.play_card()
+
+    # Add to table_cards
+    table_cards.append(c_card)
+    table_cards.append(p_card)
+
+    # Check for War!
+    # Checking the rank of the cards
+    if c_card[1] == p_card[1]:
+        war_count +=1
+        print("We have a match, time for war!")
+        print("Each player removes 3 cards 'face down' and then one card face up")
+        table_cards.extend(user.remove_war_cards())
+        table_cards.extend(comp.remove_war_cards())
+
+        # Play cards
+        c_card = comp.play_card()
+        p_card = user.play_card()
+
+        # Add to table_cards
+        table_cards.append(c_card)
+        table_cards.append(p_card)
+
+        # Check to see who had higher rank
+        if ranks.index(c_card[1]) < ranks.index(p_card[1]):
+            print(user.name+" has the higher card, adding to hand.")
+            user.hand.add(table_cards)
+        else:
+            print(comp.name+" has the higher card, adding to hand.")
+            comp.hand.add(table_cards)
+
+    else:
+        # Check to see who had higher rank
+        if ranks.index(c_card[1]) < ranks.index(p_card[1]):
+            print(user.name+" has the higher card, adding to hand.")
+            user.hand.add(table_cards)
+        else:
+            print(comp.name+" has the higher card, adding to hand.")
+            comp.hand.add(table_cards)
+
+print("Great Game, it lasted: "+str(total_rounds))
+print("A war occured "+str(war_count)+" times.")
